41a080cbcd7b16b58ab4b5b03422abd96dbbfd70
anti008 <anton.tarasenkov@mail.ru>
2021-04-01 15:01:37 +0300

    добавил мод окно

--- js/main.js
+++ js/main.js
@@ -22,3 +22,29 @@ const swiperSliderFeedback = new Swiper(".feedback-swiper-container", {
     nextEl: ".feedback-slider-button-next",
   },
 });
+
+var menuButton = document.querySelector(".header-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопке");
+  document.querySelector(".modal").classList.toggle("modal__visible");
+});
+
+var modalButton = $('[data-toggle="modal"]');
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
